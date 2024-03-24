f5db9c9a661ed21ab79108452c51931a9ba14013
kaikw <kaikw@student.ubc.ca>
2024-03-23 20:33:15 -0700

    feature: FrontEnd UI

---
+++ src/app/components/newsComponent.tsx
@@ -0,0 +1,90 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from "react";
+import SwipeBar from "./swipeBar";
+
+const exampleObject = {
+  status: "ok",
+  totalResults: 7169,
+  articles: [
+    {
+      source: {
+        id: null,
+        name: "Biztoc.com",
+      },
+      author: "finance.yahoo.com",
+      title: '2 "Magnificent Seven" Stocks to Buy Hand Over Fist Right Now',
+      description:
+        'The S&P 500 index has roared higher over the past 16 months, rebounding from its bear market low to reach record levels -- and confirm a new bull market. And the movement has been led by one group of stocks in particular. Dubbed the "Magnificent Seven" in ref…',
+      url: "https://biztoc.com/x/250ae63a7f8aa420",
+      urlToImage: "https://c.biztoc.com/p/250ae63a7f8aa420/s.webp",
+      publishedAt: "2024-03-23T21:06:08Z",
+      content:
+        "The S&amp;P 500 index has roared higher over the past 16 months, rebounding from its bear market low to reach record levels -- and confirm a new bull market. And the movement has been led by one grou… [+296 chars]",
+    },
+  ],
+};
+
+const imageUrl = exampleObject.articles[0].urlToImage;
+const newsTitle = exampleObject.articles[0].title;
+
+function NewsComponent() {
+  const [isReading, setIsReading] = useState(false);
+
+  const handleFullscreenClick = () => {
+    setIsReading(true); // Set isReading to true when FullscreenIcon is clicked
+  };
+
+  if (!isReading) {
+    return (
+      <>
+        <div className="flex justify-center mt-7 h-screen w-screen">
+          <img
+            className="rounded-3xl object-cover h-3/4 w-11/12 border-black border-2 opacity-40"
+            src={imageUrl}
+            alt="main image"
+          />
+        </div>
+        <div className="absolute ml-1 text-lg text-black top-12 left-40 underline">
+          <p className="judson-regular">Finance</p>
+        </div>
+        <div className="absolute mr-20 text-3xl text-black top-24 left-7">
+          <p className="judson-regular">{newsTitle}</p>
+        </div>
+        <div className="absolute bottom-64 left-14 ml-4">
+        <SwipeBar onFullscreenClick={handleFullscreenClick} /> {/* Pass the callback to SwipeBar */}
+        </div>
+        <div className="absolute bottom-60 left-14 ml-4 inline-flex space-x-5">
+          <div className="w-28 h-1 bg-white rounded-xl"></div>
+          <div className="w-28 h-1 bg-slate-400 rounded-xl"></div>
+        </div>
+      </>
+    );
+  } else {
+        return (
+            <>
+              <div className="flex justify-center mt-7 h-screen w-screen">
+                <img
+                  className="rounded-3xl object-cover h-3/4 w-11/12 border-black border-2 opacity-40"
+                  src={imageUrl}
+                  alt="main image"
+                />
+              </div>
+              <div className="absolute ml-1 text-lg text-black top-12 left-40 underline">
+                <p className="judson-regular">Finance</p>
+              </div>
+              <div className="absolute mr-20 text-3xl text-black top-24 left-7">
+                <p className="judson-regular">{newsTitle}</p>
+              </div>
+              <div className="absolute bottom-64 left-14 ml-4">
+              <SwipeBar onFullscreenClick={handleFullscreenClick} /> {/* Pass the callback to SwipeBar */}
+              </div>
+              <div className="absolute bottom-60 left-14 ml-4 inline-flex space-x-5">
+                <div className="w-28 h-1 bg-white rounded-xl"></div>
+                <div className="w-28 h-1 bg-slate-400 rounded-xl"></div>
+              </div>
+            </>
+    );
+  }
+}
+
+export default NewsComponent;
---
+++ src/app/components/swipeBar.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import { useSwiper } from "swiper/react";
+import Alert from '@mui/material/Alert';
+
+
+function SwipeBar({ onFullscreenClick }) {
+  const swiper = useSwiper();
+
+  const [iconColor, setIconColor] = useState('none'); // Initial color of the icon
+  const [opacityIcon, setOpacityIcon] = useState('100%'); // Initial color of the icon
+
+
+  const handleClick = () => {
+    console.log("clicked");
+
+    setIconColor('D5443D'); // Change the color to red when the icon is clicked
+    setOpacityIcon('50%'); // Change the color to red when the icon is clicked
+
+
+    setTimeout(() => {
+      setIconColor('transparent');
+      setOpacityIcon('100%')
+      swiper.slideNext();
+
+    }, 200);
+
+  };
+
+
+
+
+  return (
+    <>
+      <div className="inline-flex space-x-7">
+        <CloseIcon onClick={() => handleClick()} style={{ fontSize: 40, backgroundColor: iconColor, borderRadius: '50%', opacity: opacityIcon}} />
+        <KeyboardDoubleArrowRightIcon
+          onClick={() => {
+            setTimeout(() => {
+
+            console.log("I CLicked!!!");
+            swiper.slideNext();
+            },  200);
+          }}
+          style={{ fontSize: 40 }}
+        />
+        <FullscreenIcon style={{ fontSize: 40 }} 
+                  onClick={onFullscreenClick} // Handle FullscreenIcon click by calling the onFullscreenClick callback
+                  />
+        <BookmarkBorderIcon style={{ fontSize: 40 }} />
+      </div>
+    </>
+  );
+}
+
+export default SwipeBar;
