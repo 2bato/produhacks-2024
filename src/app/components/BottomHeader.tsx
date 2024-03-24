b92f6d6041781e0048ecc114689e6c5ba1248520
kaikw <kaikw@student.ubc.ca>
2024-03-23 21:53:34 -0700

    chore: fix icons to the correct one

--- src/app/components/BottomHeader.tsx
+++ src/app/components/BottomHeader.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import FeedIcon from '@mui/icons-material/Feed';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 function BottomHeader() {
   const [value, setValue] = useState(0);
@@ -17,9 +17,9 @@ function BottomHeader() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Home" icon={<HomeOutlinedIcon />} />
+        <BottomNavigationAction label="Feed" icon={<FeedIcon />} />
+        <BottomNavigationAction label="Profile" icon={<AccountCircleOutlinedIcon />} />
       </BottomNavigation>
     </div>
   );
